b47adfdc4cbc2db48bf67b1f85391182ff7d1f8d
jjy <jjyruby@gmail.com>
2020-05-20 18:35:26 +0800

    Wrap builtin ALWAYS_SUCCESS into Bytes

--- testtool/src/builtin.rs
+++ testtool/src/builtin.rs
@@ -1,1 +1,7 @@
-pub use ckb_always_success_script::ALWAYS_SUCCESS;
+use ckb_tool::ckb_types::bytes::Bytes;
+use lazy_static::lazy_static;
+
+lazy_static! {
+    pub static ref ALWAYS_SUCCESS: Bytes =
+        ckb_always_success_script::ALWAYS_SUCCESS.to_vec().into();
+}
