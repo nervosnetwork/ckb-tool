4293d18c9e207aa4bf7d4de6e9cd4eb0b5e47b7c
Linfeng Qian <thewawar@gmail.com>
2021-08-31 00:11:10 +0800

    chore: Support ckb2021

--- testtool/src/tests.rs
+++ testtool/src/tests.rs
@@ -1,7 +1,7 @@
 use crate::context::Context;
-use ckb_system_scripts::BUNDLED_CELL;
 use ckb_crypto::secp::{Generator, Privkey};
 use ckb_hash::{blake2b_256, new_blake2b};
+use ckb_system_scripts::BUNDLED_CELL;
 use ckb_types::{
     bytes::Bytes,
     core::{TransactionBuilder, TransactionView},
