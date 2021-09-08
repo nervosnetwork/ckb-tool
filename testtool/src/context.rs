33e7abef80c128a479e7ccc230c6edd0f27fdc64
Linfeng Qian <thewawar@gmail.com>
2021-09-08 13:45:59 +0800

    chore: update ckb dependencies to 0.100.0-rc5

--- testtool/src/context.rs
+++ testtool/src/context.rs
@@ -286,7 +286,7 @@ impl Context {
         let consensus = {
             let hardfork_switch = HardForkSwitch::new_without_any_enabled()
                 .as_builder()
-                .rfc_0232(200)
+                .rfc_0032(200)
                 .build()
                 .unwrap();
             ConsensusBuilder::default()
